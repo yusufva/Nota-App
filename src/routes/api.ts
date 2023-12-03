8a0e6f5c9a499ea1cc3d67bf7d0b9c27e846355c
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-03 21:18:04 +0700

    feature: implementing stock routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import Paths from '../constants/Paths';
 import Product from './ProductMasterRoutes';
+import Stock from './StockRoutes';
 
 
 // **** Variables **** //
@@ -10,6 +11,9 @@ const apiRouter = Router();
 
 // Add Product Master Router
 apiRouter.use(Paths.ProductMaster.Base, Product);
+
+// Add Stock Router
+apiRouter.use(Paths.Stock.Base, Stock);
 
 
 // **** Export default **** //
