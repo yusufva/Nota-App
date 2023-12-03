48c9b2f5ff03b52050664dac086a0c9ada1828e2
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-03 20:24:45 +0700

    feature: adding stock DTO

---
+++ src/Dto/StockDto.ts
@@ -0,0 +1,21 @@
+import { IStock } from '@src/models/Stock';
+
+export type StockDto = IStock;
+
+export type StockResponseDto = {
+    result: string;
+    message: string;
+    statusCode: number;
+    data: StockDto[];
+}
+
+export type NewStockResponseDto = {
+    result: string;
+    message: string;
+    statusCode: number;
+    data: StockDto | null;
+}
+
+export type NewStockRequestDto = {
+    quantity: number;
+}
