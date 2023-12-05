f7ad25c24fe426a36820a091bd03824f02ae225d
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-05 16:17:49 +0700

    feature: Implementing TX Buy Service

---
+++ src/services/TxBuyService.ts
@@ -0,0 +1,62 @@
+import TxBuyRepo from '@src/repos/TxBuyRepo';
+import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+import { NewTxBuyRequestDto, 
+	NewTxBuyResponseDto, 
+	TxBuyResponseDto } from '@src/Dto/TxBuyDto';
+import { IBoughtItems, ITxBuy } from '@src/models/TxBuy';
+
+async function GetAll(): Promise<TxBuyResponseDto>{
+	const tx : ITxBuy[] = await TxBuyRepo.GetAll();
+	const response : TxBuyResponseDto = {
+		result: 'Success',
+		message: 'Data Retrieve Successfully',
+		statusCode: HttpStatusCodes.OK,
+		data: tx,
+	};
+	return response;
+}
+
+async function GetById(id:string):Promise<TxBuyResponseDto>{
+	const tx : ITxBuy | null = await TxBuyRepo.GetById(id);
+	const response:NewTxBuyResponseDto = {
+		result: 'Success',
+		message: 'Data Retrieve Successfully',
+		statusCode: HttpStatusCodes.OK,
+		data: tx,
+	};
+	return response;
+}
+
+async function Create(tx:NewTxBuyRequestDto):Promise<NewTxBuyResponseDto>{
+	const newTxBuy:ITxBuy = {
+		id: '',
+		date: tx.date,
+		final_price: tx.final_price,
+		created_date: new Date(),
+		items: tx.items.map((item):IBoughtItems=>{
+			return {
+				id: undefined,
+				tx_id: '',
+				name: item.name,
+				buying_price: item.buying_price,
+				quantity: item.quantity,
+				total_price: item.total_price,
+				created_date: new Date(),
+			};
+		}),
+	};
+	const create = await TxBuyRepo.Create(newTxBuy);
+	const response:NewTxBuyResponseDto = {
+		result: 'Success',
+		message: 'Data Created Successfully',
+		statusCode: HttpStatusCodes.CREATED,
+		data: create,
+	};
+	return response;
+}
+
+export default{
+	GetAll,
+	GetById,
+	Create,
+} as const;
