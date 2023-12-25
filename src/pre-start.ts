8428de0d01c4bf60a64de52829b6fec771c0ae57
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-12-25 17:50:15 +0700

    fix: adding optional key value

--- src/pre-start.ts
+++ src/pre-start.ts
@@ -21,17 +21,18 @@ interface IArgs {
 
 // Command line arguments
 const args = parse<IArgs>({
-  env: {
-    type: String,
-    defaultValue: 'development',
-    alias: 'e',
-  },
+	env: {
+		type: String,
+		defaultValue: 'development',
+		alias: 'e',
+		optional: true,
+	},
 });
 
 // Set the env file
 const result2 = dotenv.config({
-  path: path.join(__dirname, `../env/${args.env}.env`),
+	path: path.join(__dirname, `../env/${args.env}.env`),
 });
 if (result2.error) {
-  throw result2.error;
+	throw result2.error;
 }
