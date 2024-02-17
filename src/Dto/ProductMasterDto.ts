b744de5eedc8b297fe9fb5496d2578626e46b5c9
yusufva <yusufvalent@gmail.com>
2024-02-17 15:21:16 +0700

    refactor: refactoring code

--- src/Dto/ProductMasterDto.ts
+++ src/Dto/ProductMasterDto.ts
@@ -1,29 +1,29 @@
-import { IProductMaster } from '@src/models/Product_Master';
+import { IProductMaster } from "@src/models/Product_Master";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export type ProductMasterDto = IProductMaster;
 
 export type ProductStockDto = {
-    quantity: number;
-}; 
+  quantity: number;
+};
 
 export type ProductMasterResponseDto = {
-    result: string;
-    message: string;
-    statusCode: number;
-    data: ProductMasterDto[];
-}
+  result: string;
+  message: string;
+  statusCode: number;
+  data: ProductMasterDto[];
+};
 
 export type NewProductMasterResponseDto = {
-    result: string;
-    message: string;
-    statusCode: number;
-    data: ProductMasterDto|null;
-}
+  result: string;
+  message: string;
+  statusCode: number;
+  data: ProductMasterDto | null;
+};
 
 export type NewProductMasterRequestDto = {
-    name: string;
-    buying_price: number;
-    selling_price: number;
-    stock: ProductStockDto|null;
-}
+  name: string;
+  buying_price: number;
+  selling_price: number;
+  stock: ProductStockDto | null;
+};
