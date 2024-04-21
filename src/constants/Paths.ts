fb08ec7e7eb82c36085cf98574182a0fcfd5f34b
yusufva <yusufvalent@gmail.com>
2024-04-21 15:16:32 +0700

    refactor: refactoring code

--- src/constants/Paths.ts
+++ src/constants/Paths.ts
@@ -2,53 +2,52 @@
  * Express router paths go here.
  */
 
-
 export default {
-	Base: '/api',
-	Users: {
-		Base: '/users',
-		Get: '/',
-		Add: '/',
-		Update: '/:id',
-		Delete: '/:id',
-	},
-	ProductMaster:{
-		Base: '/product-master',
-		Get: '/',
-		Add: '/',
-		GetById: '/:id',
-		Update: '/:id',
-		Delete: '/:id',
-	},
-	Stock:{
-		Base: '/stock',
-		Get: '/',
-		GetById: '/:id',
-		Update: '/:id',
-	},
-	TxBuy:{
-		Base: '/tx-buy',
-		Get: '/',
-		Create: '/',
-		GetById: '/:id',
-	},
-	TxSell:{
-		Base: '/tx-sell',
-		Get: '/',
-		Create: '/',
-		GetById: '/:id',
-	},
-	report:{
-		Base: '/report',
-		TxBuy: {
-			Base: '/tx-buy',
-			GetByDate: '/date',
-			GetByMonth: '/month',
-		},
-		TxSell: {
-			Base: '/tx-sell',
-			GetByDate: '/date',
-			GetByMonth: '/month',
-		},
-	},
+    Base: "/api",
+    Users: {
+        Base: "/users",
+        Get: "/",
+        Add: "/",
+        Update: "/:id",
+        Delete: "/:id",
+    },
+    ProductMaster: {
+        Base: "/product-master",
+        Get: "/",
+        Add: "/",
+        GetById: "/:id",
+        Update: "/:id",
+        Delete: "/:id",
+    },
+    Stock: {
+        Base: "/stock",
+        Get: "/",
+        GetById: "/:id",
+        Update: "/:id",
+    },
+    TxBuy: {
+        Base: "/tx-buy",
+        Get: "/",
+        Create: "/",
+        GetById: "/:id",
+    },
+    TxSell: {
+        Base: "/tx-sell",
+        Get: "/",
+        Create: "/",
+        GetById: "/:id",
+    },
+    report: {
+        Base: "/report",
+        TxBuy: {
+            Base: "/tx-buy",
+            GetByDate: "/date",
+            GetByMonth: "/month",
+        },
+        TxSell: {
+            Base: "/tx-sell",
+            GetByDate: "/date",
+            GetByMonth: "/month",
+        },
+    },
 } as const;
