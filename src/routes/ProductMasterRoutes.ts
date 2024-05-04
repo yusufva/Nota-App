4f1c95ae568d550b34324377b05520715a0e7a0b
yusufva <yusufvalent@gmail.com>
2024-05-04 10:35:16 +0700

    feature: implementing fastest validator

--- src/routes/ProductMasterRoutes.ts
+++ src/routes/ProductMasterRoutes.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable max-len */
 import HttpStatusCodes from "@src/constants/HttpStatusCodes";
 
@@ -6,10 +10,11 @@ import Paths from "@src/constants/Paths";
 import { Router } from "express";
 import { IReq, IRes } from "./types/express/misc";
 import { NewProductMasterRequestDto } from "@src/Dto/ProductMasterDto";
-import jetValidator from "jet-validator";
+import Validator, { ValidationError } from "fastest-validator";
+// import jetValidator from "jet-validator";
 
 const router = Router(),
-    validate = jetValidator();
+    v = new Validator();
 
 router.get(Paths.ProductMaster.Get, async (_: IReq, res: IRes) => {
     const products = await ProductMasterService.GetAll();
@@ -24,8 +29,35 @@ router.get(Paths.ProductMaster.GetById, async (req: IReq, res: IRes) => {
 
 router.post(
     Paths.ProductMaster.Add,
-    validate("name", ["buying_price", "number"], ["selling_price", "number"]),
     async (req: IReq<NewProductMasterRequestDto>, res: IRes) => {
+        const schema = {
+            name: { type: "string" },
+            buying_price: { type: "number" },
+            selling_price: { type: "number" },
+            stock: {
+                type: "object",
+                props: {
+                    quantity: "number",
+                    satuan: "string",
+                },
+                optional: true,
+            },
+        };
+        const validate = v.compile(schema);
+        const validation = validate(req.body) as ValidationError[] | true;
+        if (validation !== true) {
+            const errors = validation.map((error: any) => ({
+                message: error.message,
+                field: error.field,
+            }));
+            return res.status(HttpStatusCodes.BAD_REQUEST).json({
+                result: "Failed",
+                message: "Validation Failed",
+                statusCode: HttpStatusCodes.BAD_REQUEST,
+                error: errors,
+            });
+        }
+        // return res.json(validate);
         const product = req.body;
         const newProduct: NewProductMasterRequestDto = {
             name: product.name,
@@ -43,14 +75,26 @@ router.post(
 
 router.put(
     Paths.ProductMaster.Update,
-    validate(
-        "name",
-        ["buying_price", "number"],
-        ["selling_price", "number"],
-        ["stock", "number"],
-        ["id", "string", "params"]
-    ),
     async (req: IReq<NewProductMasterRequestDto>, res: IRes) => {
+        const schema = {
+            name: { type: "string" },
+            buying_price: { type: "number" },
+            selling_price: { type: "number" },
+        };
+        const validate = v.compile(schema);
+        const validation = validate(req.body) as ValidationError[] | true;
+        if (validation !== true) {
+            const errors = validation.map((error: any) => ({
+                message: error.message,
+                field: error.field,
+            }));
+            return res.status(HttpStatusCodes.BAD_REQUEST).json({
+                result: "Failed",
+                message: "Validation Failed",
+                statusCode: HttpStatusCodes.BAD_REQUEST,
+                error: errors,
+            });
+        }
         const { id } = req.params;
         const product = req.body;
         const newProduct: NewProductMasterRequestDto = {
@@ -67,14 +111,10 @@ router.put(
     }
 );
 
-router.delete(
-    Paths.ProductMaster.Delete,
-    validate(["id", "string", "params"]),
-    async (req: IReq, res: IRes) => {
-        const { id } = req.params;
-        const result = await ProductMasterService.DeleteById(id);
-        return res.status(HttpStatusCodes.OK).json(result);
-    }
-);
+router.delete(Paths.ProductMaster.Delete, async (req: IReq, res: IRes) => {
+    const { id } = req.params;
+    const result = await ProductMasterService.DeleteById(id);
+    return res.status(HttpStatusCodes.OK).json(result);
+});
 
 export default router;
